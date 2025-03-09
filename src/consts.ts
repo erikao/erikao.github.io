8f35e88d9c3570788461773bf76c6378fe6f19f3
erikao <hello@erikaowens.com>
2025-03-09 18:12:48 -0400

    fixing some defaults

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Erika Owens';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Erika loves linking people, networks, and ideas at the intersection of journalism and technology.';
